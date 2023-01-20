bff79a6137769e9e1f9747b3f12aa789b84b37b2
Jonnier <alzatejonny39@gmail.com>
2023-01-20 17:12:21 -0500

    settings

--- script/index.js
+++ script/index.js
@@ -1,8 +1,12 @@
 const navToggle = document.querySelector(".toggle") ;
 const navMenu = document.querySelector(".nav-menu") ;
+const header = document.querySelector(".header")
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle("nav-menu_visible")
+    header.style.position = "fixed";
+
+    
     if(navMenu.classList.contains("nav-menu_visible")){
         navToggle.setAttribute("arial-label", "cerrar menú")
     }
